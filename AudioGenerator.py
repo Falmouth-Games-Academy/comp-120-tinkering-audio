72eadc497321598c1d69b020f0b3133b887ffbf5
Warwick New(wn182882) <mistercheeez@hotmail.co.uk>
2015-12-23 21:11:50 +0000

    added  a melody file reader and made audio notes have a peak

--- AudioGenerator.py
+++ AudioGenerator.py
@@ -1,8 +1,5 @@
-import math
-import wave
-import struct
-import os
-import random
+import math, wave, struct, os, random
+
 
 numChannels = 1                                                        # Decides whether your in mono or stereo
 audioHz = 44100                                                        # Sample rate
@@ -87,28 +84,98 @@ def echoSound(soundList, numEcho, sampleGap, volumeDecrease):
                     soundList[sample] = newSample
     return soundList
 
-#sound = loadFile(sound3)
-#sound = echoSound(sound, 60, 1000)
-#writeToFile(sound,)
+class Melody:
+    def __init__(self, source, volume, defaultPeakLocation, maxAmplitude, hertz):
+        self.__source = source
+        self.__peak = defaultPeakLocation                              # sets where the peak in each note of the song is
+        self.__volume = volume
+        self.__maxAmplitude = maxAmplitude
+        self.__hertz = hertz
 
+    #makes a list of sin values to be converted later
+    def makeSin(self, noteFrequency, numSample):
+        sinWave = []
+        sampleRate = self.__hertz / (numSample / self.__hertz)
+        interval = 1.0/noteFrequency
+        samplesPerCycle = interval * sampleRate
+        for sample in range(0, numSample):
+            rawSample = math.sin(sample / samplesPerCycle)
+            sampleValue = rawSample * self.__volume
+            #These conditions allow you to write the file even if you peak the audio with the amplitude parameter.
+            if sampleValue > 1:
+                sampleValue = 1
+            elif sampleValue < -1:
+                sampleValue = -1
+            sinWave.append(sampleValue)
+        return sinWave
+
+    def peakSin(self, sin, peakLocation):
+        if peakLocation is False:                                      # to use default peak, peakLocation = false
+            peakLocation = self.__peak
+        peakSample = len(sin) * peakLocation
+        for sample in range(0, len(sin)):
+            if sample < peakSample:
+                factor = 1 / peakSample
+                multiplyer = sample * factor
+                if multiplyer <= 1:
+                    sin[sample] = sin[sample] * multiplyer
+            elif sample > peakSample:
+                factor = 1 / (len(sin) - peakSample)
+                multiplyer = (len(sin) - sample) * factor
+                if multiplyer <= 1:
+                    sin[sample] = sin[sample] * multiplyer
+        return sin
+
+    def stringSound(self, sound1,sound2):                              # strings one sound onto the end of another
+        for sample in range(0, len(sound2)):
+            sound1.append(sound2[sample])
+        return sound1
+
+    #reads a custom format and writes it to a .wav file
+    def readMelToSin(self, filename):
+        frequencyLib = [('-A', 440), ('A#', 466), ('B', 494), ('C', 523), ('C#', 554), ('D', 587),
+                         ('D#', 622), ('E', 659), ('F', 698), ('F#', 739), ('G', 783), ('G#', 830), ('A', 880)]
+        if type(self.__source) == str:
+            filename = str(filename)+'.mel'                                # mel stands for melody
+            file = open(filename, 'r')
+            fileList = []
+            for line in file:
+                line = line.replace('\n', '')
+                fileList.append(line)
+            for index in range(0, len(fileList)):
+                fileList[index] = fileList[index].split(',', 1)
+                fileList[index][0] = fileList[index][0].upper()
+                fileList[index][1] = int(fileList[index][1])
+            self.__source = fileList
+        for note in range(0, len(fileList)):
+            fileList[note][1] = fileList[note][1] * self.__hertz
+            for libNote in range(0, len(frequencyLib)):
+                if fileList[note][0] == frequencyLib[libNote][0]:
+                    fileList[note][0] = frequencyLib[libNote][1]
+        sin = []
+        for note in range(0, len(fileList)):
+            rawSin = self.makeSin(fileList[note][0], fileList[note][1])
+            rawSin = self.peakSin(rawSin, self.__peak)
+            sin = self.stringSound(sin, rawSin)
+        return sin
 
 
 
 #test code
-print "Loading files..."
+print 'loading clock...'
 clock = loadFile('testClock')
-print 'Making sounds...'
-sin1 = makeSin(audioHz, 220, 3*audioHz, maxAmp, amplitude)
-sin2 = makeSin(audioHz, 440, 3*audioHz, maxAmp, amplitude)
-noise = makeNoise(3*audioHz, maxAmp)
-print 'Merging...'
-clock = mergeSound(clock, sin1, 1*audioHz)
-clock = mergeSound(clock, sin2, 2*audioHz)
-clock = stringSound(clock, noise)
-print 'Adding echo...'
-clock = echoSound(clock, 10, audioHz, 0.8)
-print 'Saving '+str(len(clock))+' samples...'
-print 'This may take a while'
-writeToFile(clock, 'testOutput', numChannels, maxAmp)
-print 'Playing...'
-playFile('testOutput')
+print 'generating buffer noise...'
+white = makeNoise(audioHz, 32767)
+print 'reading melody...'
+sound = Melody('BadSong', 1, 0.1, 32767, 44100)
+sound = sound.readMelToSin('BadSong')
+print 'stringing sounds...'
+sound = stringSound(clock, sound)
+sound = stringSound(sound, white)
+print 'adding echo...'
+echoSound(sound, 20, int(0.5*audioHz), 0.9)
+print 'writing...'
+print 'this often takes a long time'
+writeToFile(sound, 'TerribleSong', 1, 32767)
+print 'hopefully playing...'
+playFile('TerribleSong')
