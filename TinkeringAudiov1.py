f203f802da1f49e7ae96817010728bd4046388c8
Oliver <oli-scales@hotmail.co.uk>
2017-11-28 16:55:01 +0000

    Updates

--- TinkeringAudiov1.py
+++ TinkeringAudiov1.py
@@ -3,7 +3,8 @@ import math
 import numpy
 import random
 
-# This section intialises pygame and sets the resolution of the monitor used
+
+# This section initialises pygame and sets the resolution of the monitor used
 pygame.init()
 pygame.display.set_mode((800, 640))
 pygame.mixer.init()
@@ -16,10 +17,12 @@ chan = 2
 volume = 40
 sample_length = 2
 
+
 # This sets definitions in order to calculate and figure out tones
 def generate_tone():
-    generate_samples = numpy.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]], dtype="int8") ##generate short tone
+    generate_samples = numpy.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]], dtype="int8")  # generate short tone
     return generate_samples
+
 
 def generate_sine_wave(frequency, samplerate, sample_length, volume):
     values = []
@@ -31,33 +34,34 @@ def generate_sine_wave(frequency, samplerate, sample_length, volume):
 
     return values
 
-def make_chord(sound1,sound2,sound3,sample_length):
-    values=[]
-    for i in range(0,sample_length):
+
+def make_chord(sound1, sound2, sound3, sample_length):
+    values = []
+    for i in range(0, sample_length):
         values.append(sound1[i])
-        if i>4000:
+        if i > 4000:
             values.append(sound1[i]+sound2[i])
-        if i>8000:
+        if i > 8000:
             values.append(sound1[i]+sound2[i]+sound3[i])
     return values
 
-samples=generate_tone()
-current_a=pygame.sndarray.make_sound(samples)
+samples = generate_tone()
+current_a = pygame.sndarray.make_sound(samples)
 
-# This exits the aapplicaiyon upon pressing the exit button and on pressing a key plays the tone
-done=False
+# This exits the application upon pressing the exit button and on pressing a key, plays the tone
+done = False
 while not done:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            done=True
-        if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_o:
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_o:
                 current_a.play(-1)
 
     pygame.display.update()
 
 # This generates a sine wave and a sound and then exits the application
-tone_values_one=generate_sine_wave(4000.0,samplerate,132000,1000.0)
-chord_values=make_chord(generate_tone, 132000)
+tone_values_one = generate_sine_wave(4000.0, samplerate, 132000, 1000.0)
+chord_values = make_chord(generate_tone, 132000)
 pygame.mixer.quit()
 pygame.quit()
