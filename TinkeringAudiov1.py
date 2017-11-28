5e03843d44232b5e1a4d93e1c0a4fab1878f598e
Oliver <oli-scales@hotmail.co.uk>
2017-11-28 16:55:01 +0000

    Final Version with added text

--- TinkeringAudiov1.py
+++ TinkeringAudiov1.py
@@ -5,8 +5,13 @@ import random
 
 # This section intialises pygame and sets the resolution of the monitor used
 pygame.init()
-pygame.display.set_mode((800, 640))
+screenbox = pygame.display.set_mode((800, 600))
 pygame.mixer.init()
+
+# This section is responsible for the text on the screen
+fontforuse = pygame.font.SysFont('Arial Black', 29) #size
+text = fontforuse.render("Press the <SPACE> key to create a tone", True, (0, 234, 0))#color
+screenbox.blit(text, (110,263))
 
 # This section defines the variables that are used
 samplew = 2
@@ -14,11 +19,12 @@ samplerate = 44100.0
 bitdepth = 2.0
 chan = 2
 volume = 40
-sample_length = 2
+sample_length = 5
 
 # This sets definitions in order to calculate and figure out tones
+#Credit to Brian and his live-code repo.
 def generate_tone():
-    generate_samples = numpy.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]], dtype="int8") ##generate short tone
+    generate_samples = numpy.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6,6]], dtype="int8")
     return generate_samples
 
 def generate_sine_wave(frequency, samplerate, sample_length, volume):
@@ -31,6 +37,7 @@ def generate_sine_wave(frequency, samplerate, sample_length, volume):
 
     return values
 
+#Credit to Brian and his live-code repo.
 def make_chord(sound1,sound2,sound3,sample_length):
     values=[]
     for i in range(0,sample_length):
@@ -44,14 +51,14 @@ def make_chord(sound1,sound2,sound3,sample_length):
 samples=generate_tone()
 current_a=pygame.sndarray.make_sound(samples)
 
-# This exits the aapplicaiyon upon pressing the exit button and on pressing a key plays the tone
+#This exits the aapplicaiyon upon pressing the exit button and on pressing a key plays the tone
 done=False
 while not done:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             done=True
         if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_o:
+            if event.key==pygame.K_SPACE:
                 current_a.play(-1)
 
     pygame.display.update()
