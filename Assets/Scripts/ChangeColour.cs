804896e67ecac1258393fa45e780425102f062bd
AdrianTofan <adrianvasile.tofan@gmail.com>
2019-11-27 23:27:07 +0000

    +Added script for colouring the buttons when click the 'BTN COLOUR' button.

--- Assets/Scripts/ChangeColour.cs
+++ Assets/Scripts/ChangeColour.cs
@@ -1,17 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using System;
+
 
 public class ChangeColour : MonoBehaviour
 {
    
+    private System.Random randomized = new System.Random();
+    private float red;
+    private float green;
+    private float blue;
+    private float divisor;
+    Color newColor;
     
+
+    void Update()
+    {
+        // divisor = Convert.ToInt32(randomized.Next(2, 5));
+        red = Random.Range(0.0f, 3.0f);
+        green = Random.Range(0.0f, 3.0f);
+        blue = Random.Range(0.0f, 3.0f);
+
+
+        //Convert.ToSingle(randomized.Next(0, 1));
+    }
+
+
     public void change_colour()
     {
+
+        newColor = new Color(red, green, blue);
+
         //GetComponent<UnityEngine.UI.Image>().color = Color.green;
 
+        /* foreach (GameObject dynamicColourObject in DynamicColourObject.list)
+             dynamicColourObject.GetComponent<UnityEngine.UI.Image>().color = Color.green;
+             */
+
         foreach (GameObject dynamicColourObject in DynamicColourObject.list)
-            dynamicColourObject.GetComponent<Renderer>().sharedMaterial.color = Color.red;
+            dynamicColourObject.GetComponent<UnityEngine.UI.Image>().color = newColor;
 
     }
     
